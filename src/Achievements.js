c48cc3af6b239610037a9c7cce72774ce1744bdf
tracey b <traceyleab@gmail.com>
2023-05-04 18:54:16 -0400

    removing toastify

--- src/Achievements.js
+++ src/Achievements.js
@@ -1,15 +1,10 @@
 import { default as sample } from './sample.svg';
-import { toast } from 'react-toastify';
-// import "react-toastify/dist/ReactToastify.css";
-
 
 export default function Achievements() {
-    
+
     return(
         <div id="achievements">
             <img src={sample} class="locked" alt="sample achievement"></img>
-            {/* <img src={thermo} class={style}></img>
-            <img src={scientist} class={style}></img> */}
         </div>
     )
 }
